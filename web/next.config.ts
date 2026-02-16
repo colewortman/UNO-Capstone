b71ae94097fa57c73a8377205f3e4617b5dd3adf
colewortman <wortcole12@gmail.com>
2026-02-16 16:37:14 -0600

    Setting up GitHub pages

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/UNO-Capstone",
 };
 
 export default nextConfig;
